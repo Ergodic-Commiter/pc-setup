bd29a078996465a4f43122e43f806f2ee1959965
Diego Villamil <diego.vbineo.com@Diego-Villamil-MacBook-Pro.local>
2024-12-16 12:48:39 -0600

    figure out json formatter and such with Eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,36 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-import json from "eslint-plugin-json";
 
+import json from "@eslint/json"; 
+import markdown from "@eslint/markdown"; 
+//import prettier from "eslint-plugin-prettier";
+//import prettierConfig from "eslint-config-prettier"; 
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-{ files: ["**/*.json"], 
-  ...json.configs["recommended"] }, 
-{ files: ["**/*.js"], 
+{ name: "Global Config", 
+  languageOptions: { 
+  globals: globals.browser } },
+pluginJs.configs.recommended,
+{ name: "JS Config", 
+  files: ["**/*.js"], 
   languageOptions: {
-    sourceType: "commonjs",
-    globals: globals.browser },
-  ...pluginJs.configs.recommended },
+    sourceType: "commonjs" } },
+{ name: "JSON Config w.Prettier",
+  language: "json/json",
+  files: ["**/*.json"], 
+  // plugins: {json, prettier},  
+  plugins: {json},  
+  rules: {
+    "no-irregular-whitespace": "off",  // Estaba fallando así que lo apagamos. 
+    "json/no-duplicate-keys": "error", 
+    // "prettier/prettier": ["error", {parser: "json"}]
+  } }, 
+{ name: "Markdown Config",  
+  language: "markdown/commonmark", 
+  files: ["**/*.md"], 
+  plugins: {markdown}, 
+  rules: {
+    "markdown/no-html": "error" } },
+// prettierConfig
 ];
