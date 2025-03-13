1fc86f06d6465d024d6325e991245d5a17c95728
Diego Villamil <diego.v@bineo.com>
2025-03-13 08:31:20 -0600

    backup repos

--- source/karabiner/update_config.sh
+++ source/karabiner/update_config.sh
@@ -1,6 +1,9 @@
 
+
+KARABINER_BIN='/Library/Application\ Support/org.pqrs/Karabiner-Elements/bin/karabiner_cli'
 KARABINER_DEFAULT="$HOME/.config/karabiner/karabiner.json"
 MY_KARABINER="$PWD/refs/my-libs/mac-apps/karabiner/my_config.json"
 
 rm $KARABINER_DEFAULT
 ln -s $MY_KARABINER $KARABINER_DEFAULT
+alias karabiner-cli="$KARABINE_BIN/karabiner_cli"
